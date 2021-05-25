bc4b5b0c0c14d12eb170556558f14ed1fe2f5959
Josh R <seventytwoapp@gmail.com>
2021-05-24 19:05:03 -0500

    Initial commit.

---
+++ ExpandableSections_SwiftUI/ExpandableSections_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableSections_SwiftUIApp.swift
+//  ExpandableSections_SwiftUI
+//
+//  Created by Josh R on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableSections_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
